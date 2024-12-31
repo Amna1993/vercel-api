74bc21f983ffa9621e2320f4040bdfe89110a596
Hafiza Amna Razzaq <hafizaamnarazzaq@Hafizas-MacBook-Pro.local>
2024-12-30 23:28:36 -0500

    index.js updated

--- api/index.js
+++ api/index.js
@@ -2,33 +2,32 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
+
+dotenv.config(); // Ensure this is called before accessing process.env
+
 const app = express();
 
+// Connect to Database
 connectDB();
+
 app.use(express.json());
 
+// Default Route
 app.get('/home', (req, res) => {
     res.status(200).json("Welcome, your app is working well");
 });
 
-app.get('/',function (req, res) {
+app.get('/', (req, res) => {
     res.status(200).json("Welcome, your app is working well");
 });
 
-
+// Product Routes
 app.use('/api/products', productRoutes);
 
-
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 module.exports = app;
-
-
-
-
-
-
-
-
-
-
-
