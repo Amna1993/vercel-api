3eaf974cf4ef7b4948d2a86daff4617edb0ce1e0
Hafiza Amna Razzaq <hafizaamnarazzaq@Hafizas-MacBook-Pro.local>
2024-12-30 23:06:42 -0500

    updated index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const productRoutes = require('./routes/productRoutes');
 const app = express();
+
+connectDB();
+app.use(express.json());
 
 app.get('/home', (req, res) => {
     res.status(200).json("Welcome, your app is working well");
@@ -9,5 +15,20 @@ app.get('/', (req, res) => {
     res.status(200).json("Welcome, your app is working well");
 });
 
-// Export the app to be used as a serverless function
+
+app.use('/api/products', productRoutes);
+
+
+
 module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
